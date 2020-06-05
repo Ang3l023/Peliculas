7181b6db07892d50d075189712917f63e6f5a8ec
Angelo Rojas <50628048+Ang3l023@users.noreply.github.com>
2020-06-04 21:50:56 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,17 +1,17 @@
 export const environment = {
   production: true,
   url: 'https://api.themoviedb.org/3',
-  apikey: 'fe6ce0354cd35bc9a0b5e30cd5deba74',
+  apikey: '',
   firebaseConfig: {
-    apiKey: 'AIzaSyDM0LP6WgpGZGoK9NxFFFgdU_z_dGABRuo',
-    authDomain: 'apppeliculas-1021a.firebaseapp.com',
-    databaseURL: 'https://apppeliculas-1021a.firebaseio.com',
-    projectId: 'apppeliculas-1021a',
-    storageBucket: 'apppeliculas-1021a.appspot.com',
-    messagingSenderId: '293833921820',
-    appId: '1:293833921820:web:3831226c673636319e16d9',
-    measurementId: 'G-WCMH5P47TE'
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
+    appId: '',
+    measurementId: ''
   },
-  uriGoogle: 'https://identitytoolkit.googleapis.com/v1/accounts:',
-  urlFire: 'https://login-app-8839f.firebaseio.com/'
+  uriGoogle: '',
+  urlFire: ''
 };
